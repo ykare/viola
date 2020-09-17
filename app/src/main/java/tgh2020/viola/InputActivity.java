cf6e89c7b64402d2f059b9dd8eb27c94dfab73dd
Yoshiyuki <y.karezaki@gmail.com>
2020-09-17 19:15:14 +0900

    勝屋さんの入力画面をマージ

--- app/src/main/java/tgh2020/viola/InputActivity.java
+++ app/src/main/java/tgh2020/viola/InputActivity.java
@@ -2,13 +2,108 @@ package tgh2020.viola;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.FileDescriptor;
+import java.io.IOException;
+import java.util.Random;
 
 public class InputActivity extends AppCompatActivity {
+    private static final int RESULT_PICK_IMAGEFILE = 1000;
+    private ImageView imageView;
+
+    private Uri memoryuri;
+
+    public int colornumber() {
+        int i;
+        i = (int) (Math.random() * 3);
+        return i;
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
+
+
+        imageView = findViewById(R.id.imageView);
+
+        findViewById(R.id.selectbutton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("image/*");
+                startActivityForResult(intent, RESULT_PICK_IMAGEFILE);
+            }
+        });
+
+        Button b = findViewById(R.id.donebutton);
+        b.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View view) {
+                        TextView etTitle = findViewById(R.id.etTitle);
+                        String title = etTitle.getText().toString();
+
+                        TextView etText = findViewById(R.id.etText);
+                        String text = etText.getText().toString();
+
+                        int i = colornumber();
+                        Memory.addMemory(new Memory(i, title, memoryuri, text));
+
+                        finish();
+
+                    }
+
+                }
+        );
+
     }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+        super.onActivityResult(requestCode, resultCode, resultData);
+        if (requestCode == RESULT_PICK_IMAGEFILE && resultCode == RESULT_OK) {
+            Uri uri = null;
+            if (resultData != null) {
+                uri = resultData.getData();
+
+                try {
+                    Bitmap bmp = getBitmapFromUri(uri);
+                    imageView.setImageBitmap(bmp);
+                    memoryuri = uri;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private Bitmap getBitmapFromUri(Uri uri) throws IOException {
+        ParcelFileDescriptor parcelFileDescriptor =
+                getContentResolver().openFileDescriptor(uri, "r");
+        FileDescriptor filedescriptor = parcelFileDescriptor.getFileDescriptor();
+        Bitmap image = BitmapFactory.decodeFileDescriptor(filedescriptor);
+        parcelFileDescriptor.close();
+        return image;
+    }
+
+
 }
+
+
+
+
+
