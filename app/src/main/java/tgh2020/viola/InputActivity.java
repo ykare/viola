2b8b7d489a6ec6083e0c70a927ceeecb9a40bb36
Yoshiyuki <y.karezaki@gmail.com>
2020-09-17 12:54:38 +0900

    Initial commit

---
+++ app/src/main/java/tgh2020/viola/InputActivity.java
@@ -0,0 +1,14 @@
+package tgh2020.viola;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InputActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input);
+    }
+}
---
+++ app/src/main/java/tgh2020/viola/MainActivity.java
@@ -0,0 +1,35 @@
+package tgh2020.viola;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button input = findViewById(R.id.btnInput);
+        input.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, InputActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button detail = findViewById(R.id.btnDetail);
+        detail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
