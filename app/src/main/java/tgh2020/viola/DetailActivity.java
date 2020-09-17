7b221e20266340053627b13c2fd04d9db2ba20fc
shiho <2844971620@g.ecc.u-tokyo.ac.jp>
2020-09-17 19:29:04 +0900

    入力結果の表示

--- app/src/main/java/tgh2020/viola/DetailActivity.java
+++ app/src/main/java/tgh2020/viola/DetailActivity.java
@@ -3,12 +3,50 @@ package tgh2020.viola;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
 
 public class DetailActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        Memory memory=new Memory(Memory.GREEN,"a",null,"q");
+
+
+        ImageView imageView = findViewById(R.id.image);
+        imageView.setImageURI(memory.getImageUri());
+
+        TextView result = findViewById(R.id.textView2);
+        //書いた内容を受け取る
+        result.setText(" Title: "+memory.getTitle());
+
+        TextView result1 = findViewById(R.id.textView);
+        //書いた内容を受け取る
+        result1.setText(" "+memory.getText());
+
+
+
+        TextView date = findViewById(R.id.textView8);
+        date.setText(" Date: "+memory.getTimestamp());
+
+        Button b = findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        }
+        );
+
+
     }
 }
