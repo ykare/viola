ddd460900cbe40bc45e1daa12b8438d97cc83e0b
shiho <2844971620@g.ecc.u-tokyo.ac.jp>
2020-09-18 13:29:30 +0900

    背景を変えた

--- app/src/main/java/tgh2020/viola/DetailActivity.java
+++ app/src/main/java/tgh2020/viola/DetailActivity.java
@@ -1,20 +1,21 @@
 package tgh2020.viola;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 
-public class DetailActivity extends AppCompatActivity {
-
-
-
+public class DetailActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +29,25 @@ public class DetailActivity extends AppCompatActivity {
             memory = (Memory) intent.getSerializableExtra("memory");
         } else {
             memory=new Memory(Memory.GREEN,"a",null,"q");
+        }
+
+        LinearLayout layout = findViewById(R.id.bubble);
+        switch(memory.getColor()) {
+            case Memory.GREEN:
+                ViewCompat.setBackgroundTintList(
+                        layout,
+                        ColorStateList.valueOf(Color.GREEN));
+                break;
+            case Memory.PINK:
+                ViewCompat.setBackgroundTintList(
+                        layout,
+                        ColorStateList.valueOf(Color.RED));
+                break;
+            case Memory.YELLOW:
+                ViewCompat.setBackgroundTintList(
+                        layout,
+                        ColorStateList.valueOf(Color.YELLOW));
+                break;
         }
 
         ImageView imageView = findViewById(R.id.image);
@@ -57,4 +77,6 @@ public class DetailActivity extends AppCompatActivity {
 
 
     }
+
+
 }
